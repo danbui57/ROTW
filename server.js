4210d33e6b2fcb4071f1b5c7035e1a9f71800d39
danbui57 <danbui57@gmail.com>
2019-05-18 15:35:40 -0400

    installs and saves npm packages to package.json file

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const express = require("express");
-
 const app = express();
+const passport = require("passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
+const env = require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
 
-app.get("/", function (req, res) {
-    res.send("Hello")
-})
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
 
-app.listen(PORT, function (err) {
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+// For Passport 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//Route to homepage
+app.get("/", (req, res) => {
+    res.send("Welcome to ROTW")
+});
+
+app.listen(PORT,(err) => {
 if (!err) {
-    console.log(" Server successfully connected to http://localhost:" + PORT);
+    console.log(`Server successfully connected to http://localhost: ${PORT}`);
 }else { 
     console.log("error:", err);
 }
